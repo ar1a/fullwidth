cc16a9362aaf2f825a19f2b76aac4a5e71762ce9
Aria Edmonds <aria@ar1as.space>
2019-01-21 03:02:16 +1100

    FIX EMERGENCY EQUALS

--- Main.hs
+++ Main.hs
@@ -11,6 +11,6 @@ fullWidthifyText =
 
 convertChar :: Int -> Int
 convertChar ch
-  | ch > 0x20 && ch < 0x7F= ch + 0xFEE0
+  | ch > 0x20 && ch < 0x7F = ch + 0xFEE0
   | ch == 0x20 = 0x3000
   | otherwise = ch
