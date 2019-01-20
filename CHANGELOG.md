567ccc0e5a9751c86d9c5e5dc9545ce3f1941cc9
Aria Edmonds <aria@ar1as.space>
2019-01-21 03:00:06 +1100

    Init

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for fullwidth
+
+## 0.1.0.0 -- YYYY-mm-dd
+
+* First version. Released on an unsuspecting world.
---
+++ Main.hs
@@ -0,0 +1,16 @@
+import Data.Char
+
+main :: IO ()
+main = do
+  input <- getContents
+  putStrLn $ fullWidthifyText input
+
+fullWidthifyText :: [Char] -> [Char]
+fullWidthifyText =
+  map (chr . convertChar . ord)
+
+convertChar :: Int -> Int
+convertChar ch
+  | ch > 0x20 && ch < 0x7F= ch + 0xFEE0
+  | ch == 0x20 = 0x3000
+  | otherwise = ch
